f64699f281a8cd56a7bc802ada1af7f1d2b326f6
Imper283 <Semen534@yandex.ru>
2023-05-10 10:04:37 +0300

    updated structure

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
+
+// Окна с которыми будем рабоать
 let main = document.getElementsByClassName("manager")[0]
+let second = document.getElementsByClassName("manager")[1]
+
+
+// Кнопочки
 let btnUpdate = document.getElementById("update")
 let btnRoot = document.getElementById("toRoot")
 let btnBack = document.getElementById("backBtn")
-let path = document.getElementById("path")
+
 let curDir = "/"
 let drawAr = []
 let selectedAr = []
@@ -11,20 +17,117 @@ let miniLog = document.getElementById("miniLog")
 let updateOn = false
 let updateTime = 6000
 
-path.value = curDir
+class managerWin {
+    #win;
+    #path;
+    constructor(win = undefined, path = undefined){
+        this.#win = win
+    }
 
-//Функция возврата на уровень директории
-function dirBack(){
-    let ar = curDir.split("/")
-    ar.splice(0,1)
-    ar.pop()
-    console.log(ar)
-    curDir = '/'+ar.join('/')
-    update().then(()=>{ render() })
+    render(){
+        // console.log("Render")
+        this.innerHTML = ''
+
+        // Создание кнопки возврата на уровень директории выше.
+        if(curDir != "/"){
+            let backElem = document.createElement('div')
+            backElem.className = 'slot'
+            backElem.textContent = '..'
+            backElem.addEventListener('click',()=>{ dirBack() })
+            this.insertBefore(backElem,null)
+
+            
+        }
+        for(let i=0;i<drawAr.length;i++){
+            let rendElem = document.createElement('div')
+
+            // Создание кнопки выделения
+            let selectBtnContainer = document.createElement("button")
+            let selectBtnImg = document.createElement("img")
+            selectBtnImg.src = "/pics/plus.svg"
+            selectBtnContainer.insertBefore(selectBtnImg,null)
+            
+            let icoBox = document.createElement('div')
+            icoBox.className = "icoBox"
+
+            let textBox = document.createElement('div')
+
+            rendElem.className = 'slot'
+
+            if(drawAr[i].isSlot()){
+                let rendImg = document.createElement('img')
+                if(drawAr[i].getType() != 'dir'){
+                    textBox.textContent = drawAr[i].getName()+'.'+drawAr[i].getType()
+                    rendImg.src = "/pics/file.svg"
+                }else{
+                    textBox.textContent = drawAr[i].getName()
+                    rendElem.addEventListener("click",(e)=>{ e.stopPropagation(); folderClick(drawAr[i].getName()) })
+                    rendImg.src = "/pics/folder.svg"
+                }
+                rendImg.draggable = false
+                icoBox.insertBefore(rendImg,null)
+            }
+            rendElem.insertBefore(selectBtnContainer,null)
+            rendElem.insertBefore(icoBox,null)
+            rendElem.insertBefore(textBox,null)
+            this.insertBefore(rendElem, null)
+            
+        }
+    }
+
 }
+
+class path {
+    #domInput;
+    #dir;
+
+    constructor(domInput = undefined, dir = ''){
+        this.#domInput = domInput
+        this.#dir = dir
+    }
+
+    setText(txt){
+        this.#domInput.textContent = txt
+    }
+
+    getDir(){
+        return this.#dir
+    }
+
+}
+
+class file {
+    #name;
+    #type;
+    #path;
+    constructor(name = "undefined", type="", path=""){
+        this.#name = name
+        this.#type = type
+        this.#path = path
+    }
+    isSlot(){
+        return true
+    }
+    getName(){
+        return this.#name
+    }
+    getType(){
+        return this.#type
+    }
+}
+class empty {
+    isSlot(){
+        return false
+    }
+}
+
+path.value = curDir
 
 //Функция для обновления данных о файлах в директории
 async function update(){
+
+    let drawAr = []
+
     try {
         let res = await fetch("http://localhost:5001/pwd", {method: "POST", body: curDir})
         res = await res.text()
@@ -56,111 +159,52 @@ async function update(){
         }
         
     }
+
+    return drawAr
+
 }
 
 // Обновление содержимого по таймеру
 // setInterval(() => { update() }, updateTime);
 
+// Дальше идут менее критичные для функционала штуки
 
-// Привязка функций к кнопкам
-
-btnUpdate.addEventListener('click',async ()=>{
-    update().then(()=>{ render() })
-})
-btnRoot.addEventListener('click',async ()=>{
-    curDir = "/"
-    update().then(()=>{ render() })
-})
-btnBack.addEventListener('click', ()=>{
-    dirBack()
-})
-
-//Класс отвечающий за наличие файла.
-class file {
-    #name;
-    //Контент можно использовать для читаемых файлов, не мне лень это реализовывать.
-    #type;
-    #path;
-    constructor(name = "undefined", type="", path=""){
-        this.#name = name
-        this.#type = type
-        this.#path = path
-    }
-    isSlot(){
-        return true
-    }
-    getName(){
-        return this.#name
-    }
-    getType(){
-        return this.#type
-    }
-}
-class empty {
-    isSlot(){
-        return false
-    }
+function dirBack(){
+    let ar = curDir.split("/")
+    ar.splice(0,1)
+    ar.pop()
+    console.log(ar)
+    curDir = '/'+ar.join('/')
+    update().then(()=>{ render(main) })
 }
 
 function folderClick(name){
     if(curDir != '/'){
         curDir = curDir+'/'+name 
-    }else{
+    }else{  
         curDir = curDir+name
     }
-    update().then(()=>{ render() })
+    update().then(()=>{ render(main) })
 }
 
-function render(){
-    // console.log("Render")
-    main.innerHTML = ''
+// Привязка функций к кнопкам
+btnUpdate.addEventListener('click',async ()=>{
+    update().then(()=>{ render(main) })
+})
+btnRoot.addEventListener('click',async ()=>{
+    curDir = "/"
+    update().then(()=>{ render(main) })
+})
+btnBack.addEventListener('click', ()=>{
+    dirBack()
+})
 
-    // Создание кнопки возврата на уровень директории выше.
-    if(curDir != "/"){
-        let backElem = document.createElement('div')
-        backElem.className = 'slot'
-        backElem.textContent = '..'
-        backElem.addEventListener('click',()=>{ dirBack() })
-        main.insertBefore(backElem,null)
 
-        
-    }
-    for(let i=0;i<drawAr.length;i++){
-        let rendElem = document.createElement('div')
 
-        // Создание кнопки выделения
-        let selectBtnContainer = document.createElement("button")
-        let selectBtnImg = document.createElement("img")
-        selectBtnImg.src = "/pics/plus.svg"
-        selectBtnContainer.insertBefore(selectBtnImg,null)
-        
-        let icoBox = document.createElement('div')
-        icoBox.className = "icoBox"
 
-        let textBox = document.createElement('div')
 
-        rendElem.className = 'slot'
 
-        if(drawAr[i].isSlot()){
-            let rendImg = document.createElement('img')
-            if(drawAr[i].getType() != 'dir'){
-                textBox.textContent = drawAr[i].getName()+'.'+drawAr[i].getType()
-                rendImg.src = "/pics/file.svg"
-            }else{
-                textBox.textContent = drawAr[i].getName()
-                rendElem.addEventListener("click",(e)=>{ e.stopPropagation(); folderClick(drawAr[i].getName()) })
-                rendImg.src = "/pics/folder.svg"
-            }
-            rendImg.draggable = false
-            icoBox.insertBefore(rendImg,null)
-        }
-        rendElem.insertBefore(icoBox,null)
-        rendElem.insertBefore(textBox,null)
-        rendElem.insertBefore(selectBtnContainer,null)
-        main.insertBefore(rendElem, null)
-        
-    }
-}
+
 
 
 
