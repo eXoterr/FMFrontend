437c9f843be4838b0c026a5cb69e740cf017874e
eXoterr <stellarium@exoterr.anonaddy.me>
2023-05-17 10:27:14 +0300

    api

--- api.js
+++ api.js
@@ -2,11 +2,26 @@ class API{
 	static #urls = {
 		baseUrl: "http://localhost:5001/",
 		dirsPath: "pwd",
-		mkdirPath: "mkdir"
+		mkdirPath: "mkdir",
+		existsPath: "exists",
+		removePath: "rm",
+		movePath: "mv"
 	}
 
 	static async createDir(dirPath){
 		return await this.#fetchFromBackend(this.#urls.mkdirPath, dirPath)
+	}
+
+	static async move(dirPath){
+		return await this.#fetchFromBackend(this.#urls.movePath, dirPath)
+	}
+
+	static async pathExists(dirPath){
+		return await this.#fetchFromBackend(this.#urls.existsPath, dirPath)
+	}
+
+	static async remove(dirPath){
+		return await this.#fetchFromBackend(this.#urls.removePath, dirPath)
 	}
 
 	static async getDirs(dirPath){
